ce1657550e4ec7bd3d1ba77baae5666cd2efb371
QCS-Theory <theory@qcs.cit.tum.de>
2024-08-30 12:57:50 +0200

    test

--- awake.py
+++ awake.py
@@ -1,7 +1,12 @@
 import time
 import requests
 
-a = 1
-b = 2
+# Define the file name
+file_name = "example.txt"
 
-print(a+b)
+# Open the file in write mode ('w') which creates the file if it doesn't exist
+with open(file_name, 'w') as file:
+    # Write some text into the file
+    file.write("Hello, this is a sample text!")
+
+print(f"File '{file_name}' has been created and text has been written into it.")
