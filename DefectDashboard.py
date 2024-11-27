b72fae93e6780213e1bb140f89da3026fd826bb7
QCS-Theory <theory@qcs.cit.tum.de>
2024-11-27 16:52:46 +0100

    unit

--- DefectDashboard.py
+++ DefectDashboard.py
@@ -886,6 +886,7 @@ for tabs in tab_selection:
                     Photophysical_properties["ZPL (nm)"]=Photophysical_properties["ZPL (nm)"].astype(int)
                     Photophysical_properties["Lifetime (ns)"]=Photophysical_properties["Lifetime (ns)"].astype(int)
                     Photophysical_properties["Lifetime (ns)"] = Photophysical_properties["Lifetime (ns)"].map("{:.2E}".format)
+                    Photophysical_properties["Configuration coordinate (amu^(1/2) \AA)"]=Photophysical_properties["Configuration coordinate (amu^(1/2) \AA)"]
 
                     try: 
                         ppdefects = Photophysical_properties[(Photophysical_properties['Defect'] == str_defect) & (Photophysical_properties['Charge state'] ==chargetrans[str_charge])]
@@ -894,7 +895,7 @@ for tabs in tab_selection:
                     except  KeyError:
                         ppdefects = Photophysical_properties[Photophysical_properties['Defect'] == str_defect]
                     emp=ppdefects.iloc[:,3:]
-                    emp.rename(columns={"dipole_x":"µₓ (Debye)","dipole_y":"μᵧ (Debye)","dipole_z":"µz (Debye)","Intensity":"Intensity (Debye)","Angle of emission dipole wrt the crystal axis":"Angle of emission dipole wrt the crystal axis (degree)"},inplace=True)
+                    emp.rename(columns={"dipole_x":"µₓ (Debye)","dipole_y":"μᵧ (Debye)","dipole_z":"µz (Debye)","Intensity":"Intensity (Debye)","Angle of emission dipole wrt the crystal axis":"Angle of emission dipole wrt the crystal axis (degree)","Configuration coordinate (amu^(1/2) \AA)":"Configuration coordinate (amu^(1/2) Å)"},inplace=True)
                     emp=emp.T
                     jj =1
                     newheadcol =[]
